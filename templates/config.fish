c27b5658f02ea3d7f212c4db8d27f6b6746b33d3
Matt McFadyen <mmcfadyen91@gmail.com>
2015-04-24 11:23:53 -0400

    :fish: my :tropical_fish: config

--- templates/config.fish
+++ templates/config.fish
@@ -2,12 +2,12 @@
 set fish_path $HOME/.oh-my-fish
 
 # Theme
-set fish_theme robbyrussell
+set fish_theme bobthefish
 
 # All built-in plugins can be found at ~/.oh-my-fish/plugins/
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme
+set fish_plugins theme matts-custom-plugins emojiclock brew sublime
 
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
